29b2cec463cefb33f59a8ccfe1a186ee4ada6127
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-10-14 11:45:32 -0700

    menyelesaikan video 5 model collection dan controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>WPU BLOG | HOME</title>
-</head>
-<body>
-    <h1> HALAMAN HOME </h1>
-<script src="js/script.js"></script>
-</body>
+@extends('layouts.main')
 
-</html>
+@section('container')
+<h1>Halaman Home</h1>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,71 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "Home"
+
+    ]);
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Anjara darojatun nisa",
+        "email" => "193040006.anjara@mail.unpas.ac.id",
+        "image" => "profile.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aliquid consectetur temporibus. Facilis similique ut nostrum quod rerum. Exercitationem officia saepe labore omnis ut eligendi aspernatur dignissimos ipsum blanditiis enim."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doddy Ferdiansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam temporibus, nesciunt perferendis quidem accusamus dicta dolores voluptas distinctio dignissimos suscipit! Inventore explicabo modi quidem maiores hic voluptatem accusamus, debitis porro quaerat temporibus veniam suscipit blanditiis corporis alias minus amet sapiente assumenda similique voluptatum, odio numquam perspiciatis? Voluptate in nam reprehenderit. Minus, vitae, enim dignissimos soluta cumque esse suscipit labore, iste ut ab animi quo? Debitis maiores tenetur deserunt aliquam alias magnam expedita aut laudantium vel repellendus? Autem provident qui laborum repellendus a, ducimus cumque nisi totam quae, corrupti quod? Vel repudiandae ratione laudantium reprehenderit ab aut assumenda non hic molestiae."
+        ]
+        ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+    
+});
+
+// halman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aliquid consectetur temporibus. Facilis similique ut nostrum quod rerum. Exercitationem officia saepe labore omnis ut eligendi aspernatur dignissimos ipsum blanditiis enim."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doddy Ferdiansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam temporibus, nesciunt perferendis quidem accusamus dicta dolores voluptas distinctio dignissimos suscipit! Inventore explicabo modi quidem maiores hic voluptatem accusamus, debitis porro quaerat temporibus veniam suscipit blanditiis corporis alias minus amet sapiente assumenda similique voluptatum, odio numquam perspiciatis? Voluptate in nam reprehenderit. Minus, vitae, enim dignissimos soluta cumque esse suscipit labore, iste ut ab animi quo? Debitis maiores tenetur deserunt aliquam alias magnam expedita aut laudantium vel repellendus? Autem provident qui laborum repellendus a, ducimus cumque nisi totam quae, corrupti quod? Vel repudiandae ratione laudantium reprehenderit ab aut assumenda non hic molestiae."
+        ]
+        ];
+
+        $new_post = [];
+        foreach($blog_posts as $post) {
+            if($post["slug"] === $slug) {
+                $new_post = $post;
+
+            }
+        }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
+});
+
