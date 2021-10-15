6558ab36b21cb0958677cc6f529f1dd01b9edf25
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-10-14 22:34:08 -0700

    menyelesaikan video ke depalan post category dan eloquent relationship

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+
+use App\Models\Category;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -34,4 +36,19 @@ Route::get('/blog', [PostController::class, 'index']);
 
 // halman single post
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function(){
+    return view('categories',[
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
 
