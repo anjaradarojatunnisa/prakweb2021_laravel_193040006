1e96e44d5ea57290e610ce8e82f2a5b0dcfae5ac
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-10-21 08:01:59 -0700

    N+1 DAN problem

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use app\models\user;
+use app\models\category;
+use app\models\post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,95 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
+ 
+
+    //user::create([
+       // 'name' => 'sandhika galih',
+       // 'email' => 'sandhikagalih@gmail.com',
+        //'//password' =>bcrypt('12345')
+    //]);
+    //user::create([
+      //  'name' => 'doddy',
+        //'email' => 'doddy@gmail.com',
+        //'password' =>bcrypt('12345')
+    //]);
+
+ //   User::factory(5)->create();
+    Post::factory(20)->create();
+
+
+    category::create([
+        'name' => 'web programming',
+        'slug' => 'web-programming'
+        
+    ]);
+    category::create([
+        'name' => 'personal',
+        'slug' => 'personal'
+        
+    ]);
+
+    
+
+    //post::create([
+      //  'title' =>'judul pertama',
+        //'slug' =>'judul-pertama',
+       // 'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+       //  quis autem ipsam eum. Alias, perferendis eius beatae autem velit fugit soluta 
+        // officia dolore cumque repellat debitis optio culpa delectus rem saepe cupiditate. 
+         //Eveniet praesentium tempore magnam quae saepe totam accusantium quia quod qui porro 
+       //  autem veritatis blanditiis animi fugit unde obcaecati, dolorem, sint dignissimos! Officiis,
+        // sunt iure rerum provident iusto cumque tempore impedit veniam obcaecati voluptates, 
+        // aperiam perspiciatis eum fugit natus ipsa? Excepturi facilis vel sequi eveniet ea quo fugit accusamus,
+        //  nostrum adipisci quas dolorum quae accusantium id dolorem in nihil odit pariatur sint provident fuga incidunt? Repellat, cumque beatae!',
+        //'category_id' => 1,
+        //'user_id' => 1
+
+    //])
+    //post::create([
+     //   'title' =>'judul kedua',
+      //  'slug' =>'judul-ke-dua',
+     //   'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+     //    quis autem ipsam eum. Alias, perferendis eius beatae autem velit fugit soluta 
+     //    officia dolore cumque repellat debitis optio culpa delectus rem saepe cupiditate. 
+      //   Eveniet praesentium tempore magnam quae saepe totam accusantium quia quod qui porro 
+      //   autem veritatis blanditiis animi fugit unde obcaecati, dolorem, sint dignissimos! Officiis,
+      //   sunt iure rerum provident iusto cumque tempore impedit veniam obcaecati voluptates, 
+       //  aperiam perspiciatis eum fugit natus ipsa? Excepturi facilis vel sequi eveniet ea quo fugit accusamus,
+        //  nostrum adipisci quas dolorum quae accusantium id dolorem in nihil odit pariatur sint provident fuga incidunt? Repellat, cumque beatae!',
+        //'category_id' => 1,
+        //'user_id' => 1
+        //])
+
+      //  post::create([
+      //      'title' =>'judul ke tiga',
+      //      'slug' =>'judul-ke-tiga',
+      //      'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      //       quis autem ipsam eum. Alias, perferendis eius beatae autem velit fugit soluta 
+      //       officia dolore cumque repellat debitis optio culpa delectus rem saepe cupiditate. 
+       //      Eveniet praesentium tempore magnam quae saepe totam accusantium quia quod qui porro 
+      //       autem veritatis blanditiis animi fugit unde obcaecati, dolorem, sint dignissimos! Officiis,
+      //       sunt iure rerum provident iusto cumque tempore impedit veniam obcaecati voluptates, 
+       //      aperiam perspiciatis eum fugit natus ipsa? Excepturi facilis vel sequi eveniet ea quo fugit accusamus,
+       //       nostrum adipisci quas dolorum quae accusantium id dolorem in nihil odit pariatur sint provident fuga incidunt? Repellat, cumque beatae!',
+      //      'category_id' => 1,
+       //     'user_id' => 1
+      //      ])
+           // post::create([
+             //   'title' =>'judul ke empat',
+               // 'slug' =>'judul-ke-empat',
+              //  'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              //   quis autem ipsam eum. Alias, perferendis eius beatae autem velit fugit soluta 
+             //    officia dolore cumque repellat debitis optio culpa delectus rem saepe cupiditate. 
+            //     Eveniet praesentium tempore magnam quae saepe totam accusantium quia quod qui porro 
+             //    autem veritatis blanditiis animi fugit unde obcaecati, dolorem, sint dignissimos! Officiis,
+             //    sunt iure rerum provident iusto cumque tempore impedit veniam obcaecati voluptates, 
+            //  aperiam perspiciatis eum fugit natus ipsa? Excepturi facilis vel sequi eveniet ea quo fugit accusamus,
+              // nostrum adipisci quas dolorum quae accusantium id dolorem in nihil odit pariatur sint provident fuga incidunt? Repellat, cumque beatae!',
+         //       'category_id' => 2,
+       //         'user_id' => 2
+     //           ])
+    
     }
 }
