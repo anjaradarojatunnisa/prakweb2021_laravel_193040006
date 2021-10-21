0d7823480786c10fea9a2662e16bfecc54298806
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-10-21 07:50:47 -0700

    factory dan faker

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,14 +10,17 @@ class PostController extends Controller
     public function index()
     {   
     return view('posts', [
-        "title" => "Posts",
-        "posts" => Post::all()
+        "title" => "All Posts",
+        "Active" =>'posts'
+       // "posts" => Post::all()
+        "posts" => Post::latest()->get()
     ]);
     }
      public function show(Post $post)
      {
         return view('post', [
             "title" => "Single Post",
+            "Active" =>'posts'
             "post" => $post
         ]);
      }
