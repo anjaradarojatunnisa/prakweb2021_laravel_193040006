5b9ca49f7c099c73f443692747590eb455afdbc9
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-10-27 17:34:13 -0700

    menyelesaikan video 14 login dan registation

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Models\Post;
+use App\Http\Controllers\DashboardController;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-use App\Models\Category;
-use App\Models\user;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -45,6 +47,16 @@ Route::get('/categories', function(){
         'categories' => Category::all()
     ]);
 });
+//login 
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+//Register
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+//Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+
 
 //Route::get('/categories/{category:slug}', function(Category $category){
     //return view('posts', [
