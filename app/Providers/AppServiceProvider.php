cda8d2126cb75278d67d82816ea0808e76a93808
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-11-30 07:38:29 -0800

    Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        paginatior::useBootstrap();
+        Paginator::useBootstrap();
+        Gate::define('admin', function(User $user) {
+            return $user->is_admin;
+        });
     }
 }
