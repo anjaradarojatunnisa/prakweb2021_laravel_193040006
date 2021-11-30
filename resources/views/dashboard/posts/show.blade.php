56564c05c6c39b0bfac150bfeb2ada71e0a663f6
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-11-30 03:51:55 -0800

    menyelesaikan video 21 upload image

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -21,6 +21,9 @@
             @if($post->image)
             <div style="max-height: 350px; overflow:hidden;">
             <img src="{{ asset('storage/'. $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+            </div>           
+            <div style="max-height: 350px; overflow:hidden;">
+            <img src="{{ asset('storage/'. $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
             </div>
                 
             @else
