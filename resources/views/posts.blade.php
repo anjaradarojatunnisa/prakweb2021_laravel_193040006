80bf5fa5d6481f4ce84314ea03c998d5ee4e344a
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-11-30 03:53:50 -0800

    menyelesaikan video 21 upload image

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -23,7 +23,18 @@
 
 @if ($posts->count())
     <div class="card mb-3">
-    <img src="https://source.unsplash.com/1280x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+    @if($post{[0]->image)
+            <div style="max-height: 350px; overflow:hidden;">
+            <img src="{{ asset('storage/'. $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid ">
+            </div>           
+            <div style="max-height: 350px; overflow:hidden;">
+            <img src="{{ asset('storage/'. $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+            </div>
+                
+            @else
+            <img src="https://source.unsplash.com/1280x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+            @endif
+    
     <div class="card-body text-center">
         <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
         <p>
@@ -43,7 +54,12 @@
             <div class="col-md-4 mb-3">
                 <div class="card">
                     <div class="position-absolute px-3 py-2 " style="background-color: rgba(0, 0, 0, 0.7);"><a href="/posts?category={{$post->category->slug}}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
-                <img src="https://source.unsplash.com/500x500?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                    @if($post->image>
+            <img src="{{ asset('storage/'. $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid ">
+            @else
+            <img src="https://source.unsplash.com/500x500?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+            @endif
+                
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <p>
