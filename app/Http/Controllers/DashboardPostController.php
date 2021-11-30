669c6ba46edfd5395a8e65e9f9d3123970d9ea00
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-11-29 22:29:10 -0800

    menyelesaikan video 20 update dan delete post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -123,7 +123,7 @@ class DashboardPostController extends Controller
     public function destroy(Post $post)
     {
         Post::destroy($post->id);
-        return redirect('/dashboard/posts')->with('success', 'Post has been added!');
+        return redirect('/dashboard/posts')->with('success', 'Post has been update!');
     }
 
     public function checkSlug(Request $request)
