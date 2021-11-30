d5b27b0a3bbb92abd5091922443146f46f8bb7b5
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-11-30 03:48:02 -0800

    menyelesaikan video 21 upload image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -46,9 +46,13 @@ class DashboardPostController extends Controller
             'title' => 'required|max:255',
             'slug' => 'required|unique:post',
             'category_id' => 'required',
+            'image' => 'image|file|max:50000',
             'body' => 'required'
         ]);
+        if($request->file('image')) {
+            $validateData['image'] = $request->file('image')->store('post-images');
 
+        }
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
