5e31385857efc2863dde77f81a8423c7cd2fd832
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-11-30 04:18:04 -0800

    menyelesaikan video 21 preview, upload dan delete

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -6,7 +6,7 @@
   </div>   
 
   <div class="col-lg-8">
-      <form method="post" action="/dashboard/posts" class="mb-5">
+        <form method="POST" action="/dashboard/posts" class="mb-5" enctype="multipart/form-data">
         @csrf
         <div class="mb-3">
           <label for="title" class="form-label">Title</label>
@@ -39,29 +39,52 @@
                 @endforeach
               </select>
           </div>
-        <div class="mb-3">
-            <label for="body" class="form-label">Body</label>
-            @error('body')
-            <p class="text-danger">{{ $message }}</p>    
-            @enderror
-            <input id="body" type="hidden" name="body" value="{{ old('body') }}">            <trix-editor input="body"></trix-editor>
-          </div>
+          <div class="mb-3">
+                <label for="image" class="form-label">Post Image</label>
+                <img class="img-preview img-fluid mb-3 col-sm-5">
+                <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" 
+                onchange="previewImage()">
+                @error('image')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="body" class="form-label">Body</label>
+                @error('body')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+                <input id="body" type="hidden" name="body" value="{{ old('body') }}">
+                <trix-editor input="body"></trix-editor>
+            </div>
 
-        <button type="submit" class="btn btn-primary">Create Post</button>
-      </form>
-  </div>
 
-  <script>
-      const title = document.querySelector('#title');
-      const slug = document.querySelector('#slug');
-      title.addEventlistener('change', function() {
-        fetch('/dashboard/posts/checkSlug?title=' + title.value)
-        .then(response => response.json())
-        .then(data => slug.value = data.slug)
-      });
-      document.addEventlistener('trix-file-accept', function(e){
-          e.preventDefault();
-      });
-  </script>
+            <button type="submit" class="btn btn-primary">Create Post</button>
+        </form>
+    </div>
 
-@endsection 
+    <script>
+        const title = document.querySelector('#title')
+        const slug = document.querySelector('#slug')
+        title.addEventListener('change', function() {
+            fetch('/dashboard/posts/checkSlug?title=' + title.value)
+            .then(response => response.json())
+            .then(data => slug.value = data.slug)
+        });
+        document.addEventListener('trix-file-accept', function(e) {
+            e.preventDefault();
+        })
+        function previewImage() {
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+            imgPreview.style.display = 'block';
+            const oFRead = new FileReader();
+            oFRead.readAsDataURL(image.files[0]) 
+            oFRead.onload = function(oFREvent) {
+                imgPreview.src = oFREvent.target.result;
+            }
+        }
+       
+    </script>
+@endsection
